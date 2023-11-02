2abdd85d19d08efa3ddfdfa8bc95e69df0881411
SebaMendez13 <se.mendez27@gmail.com>
2023-11-02 03:14:10 -0300

    footer final

--- client/src/pages/component/footer.jsx
+++ client/src/pages/component/footer.jsx
@@ -6,19 +6,12 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-content">
         <div className="footer-logo">
-          <img src="/logo.png" alt="Aventuras Logo" />
-          <h1>Aventuras</h1>
+          <img src={require('../assets/img/l7.jpg')} alt="Mi Imagen"/>
         </div>
-        <img src="../assets/img/l7.jpg" />
         <div className="footer-links">
-          <a href="/about">Acerca de</a>
+          <a href="/about">Acerca de Nosotros</a>
           <a href="/contact">Contacto</a>
           <a href="/adventures">Aventuras</a>
-        </div>
-        <div className="footer-social">
-          <a href="https://facebook.com"><i className="fab fa-facebook"></i></a>
-          <a href="https://twitter.com"><i className="fab fa-twitter"></i></a>
-          <a href="https://instagram.com"><i className="fab fa-instagram"></i></a>
         </div>
       </div>
     </div>
