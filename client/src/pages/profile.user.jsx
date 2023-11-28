157f584a2e50cbd5eb2cceacf245382639462f4d
Gabolara453 <holaqace453@gmail.com>
2023-11-28 18:41:08 -0300

    cambios en home y perfil user

--- client/src/pages/profile.user.jsx
+++ client/src/pages/profile.user.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Data from "./prueba.json";
 import './css/profile.user.css';
 import imagen from "./assets/aa.jpg"
+import { useAuth } from "../context/authContext";
+import { infoUser } from "../context/auth.backend";
 import Slidenavuser from "./component/slidenavuser";
 
 
 const UserProfile = () => {
+  const auth =  useAuth();
+
+  const id = auth.id;
+  console.log(id);
+
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    if(!id) return;
+    infoUser(id).then(data => {
+      const response = data.response;
+      setUser(response);
+    })
+  }, [id])
+
+  console.log(user);
+
+    
+
   return (
     <><div className="public">
       <div className="public_nav">
